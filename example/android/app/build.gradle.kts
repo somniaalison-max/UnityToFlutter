abeb806012acaf88824bd440173407edd5e813f6
somniaalison-max <somniaalison@gmail.com>
2026-02-28 19:07:33 +0300

    Trigger build

--- example/android/app/build.gradle.kts
+++ example/android/app/build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 android {
     namespace = "com.learntoflutter.flutter_embed_unity_android"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = "27.0.12077973"
+    ndkVersion = "27.2.12479018"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
